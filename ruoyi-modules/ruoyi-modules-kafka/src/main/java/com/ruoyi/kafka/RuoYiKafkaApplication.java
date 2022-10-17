546e646a50a8f23b9dabc8fa3d42bdea463d98a2
wangyuan <wangyuan@lvmama.com>
2022-10-17 17:59:01 +0800

    kfk module

---
+++ ruoyi-modules/ruoyi-modules-kafka/src/main/java/com/ruoyi/kafka/RuoYiKafkaApplication.java
@@ -0,0 +1,17 @@
+package com.ruoyi.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * kfk 模块
+ *
+ * @date 2022/10/17 16:20
+ */
+@SpringBootApplication
+public class RuoYiKafkaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RuoYiKafkaApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  kfk 模块启动成功   ლ(´ڡ`ლ)ﾞ  \n");
+    }
+}
---
+++ ruoyi-modules/ruoyi-modules-kafka/src/main/java/com/ruoyi/kafka/controller/MyKfkConsumer.java
@@ -0,0 +1,34 @@
+package com.ruoyi.kafka.controller;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+/**
+ * kafka 消费者
+ *
+ * @date 2022/10/17 16:23
+ */
+@Component
+public class MyKfkConsumer {
+
+    // kafka 的监听器 配置其 topic 和 消费者组
+    @KafkaListener(topics = "default.specific.orderDetail", groupId = "dso1")
+    public void dso1(ConsumerRecord<String, String> record, Acknowledgment ack) {
+        System.out.println("dso1 topics=default.specific.orderDetail > groupId= >>>");
+        System.out.println("dso1 record >> " + record);
+        System.out.println("dso1 record.value() >> " + record.value());
+        //手动提交offset
+        ack.acknowledge();
+    }
+
+    // 配置多个消费组
+    @KafkaListener(topics = "default.specific.orderDetail", groupId = "dso2")
+    public void dso2(ConsumerRecord<String, String> record, Acknowledgment ack) {
+        System.out.println("dso2 topics=default.specific.orderDetail > groupId=dso2 >>>");
+        System.out.println("dso2 record >> " + record);
+        System.out.println("dso2 record.value() >> " + record.value());
+        ack.acknowledge();
+    }
+}
